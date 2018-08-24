66134758439eb975f1012b91c180c8b5918f9ff4
Konstantin Papesh <konstantin@sydoxx.com>
2018-08-24 12:53:44 +0200

    Merged with class-rebuild

---
+++ FileExtensionManager.h
@@ -0,0 +1,15 @@
+//Multiple Inclusion Protection
+#ifndef FILEEXTENSIONMANAGER_H
+#define FILEEXTENSIONMANAGER_H
+
+
+#include "Object.h"
+class FileExtensionManager :
+	public Object
+{
+public:
+	FileExtensionManager();
+	~FileExtensionManager();
+};
+
+#endif//FILEEXTENSIONMANAGER_H
